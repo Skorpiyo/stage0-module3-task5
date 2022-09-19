f17ff5963e9f9e134d8b458ec481847faf989e8f
Skorpiyo <skorpio277@gmail,com>
2022-09-19 18:22:02 +0300

    convert: (float minutes)                  float second;                  minutes = 11.5f;                  second = minutes*60;  threestuckvar:System.out.println("" +first +second +third);  NumberDivider:         float sum = 19f;                         sum /= 5;

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float sum = 19f;
+        sum /= 5;
+        System.out.println(sum);
     }
 }
